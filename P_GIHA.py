806d913d59d734b8d1a2d22e6c65a8568a1ce5b4
mohsen1amiri <mohsenamiri.work.ac@gmail.com>
2025-12-26 18:46:31 +0100

    Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.

--- P_GIHA.py
+++ P_GIHA.py
@@ -9,16 +9,12 @@ class P_GIHA:
       - References: Theorem 8.1 .
     """
 
-<<<<<<< HEAD
-    def __init__(self, paths, feature_matrix, m, d, lambda_reg=1.0, epsilon=0.01, delta=0.05, R=1.0, S_0=1.0):
-=======
     def __init__(
     self, paths, feature_matrix, m, d,
     lambda_reg=1.0, epsilon=0.01, delta=0.05, R=1.0, S_0=1.0,
     step_pool_mode="paths"  # "paths" (union of path steps) or "all"
     ):  
 
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
         """
         Args:
             paths (dict): Mapping of path_id -> list of step_indices.
@@ -34,25 +30,19 @@ class P_GIHA:
         self.paths = paths
         self.features = feature_matrix
         self.m = m
-<<<<<<< HEAD
-=======
         if m <= 0:
             raise ValueError("m must be >= 1")
         if m >= len(paths):
             raise ValueError("m must be < number of paths")
 
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
         self.d = d
         self.epsilon = epsilon
         self.delta = delta
         self.lambda_reg = lambda_reg
         self.R = R
         self.S_0 = S_0
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
         
         # Initialize Shared Linear Model Parameters
         # V_0 = lambda * I, so V_inv = (1/lambda) * I
@@ -65,10 +55,6 @@ class P_GIHA:
             path_feats = self.features[step_indices]
             self.g_pi[pid] = np.mean(path_feats, axis=0)
 
-<<<<<<< HEAD
-        self.t = 1
-        self.num_unique_steps = feature_matrix.shape[0]
-=======
         self.t = 0  # iteration counter
         self.num_unique_steps = feature_matrix.shape[0]
         # --- Step pool (two options) ---
@@ -89,7 +75,6 @@ class P_GIHA:
             raise ValueError("step_pool_mode must be 'paths' or 'all'")
 
 
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
 
     def _get_confidence_radius_beta(self):
         """
@@ -139,48 +124,6 @@ class P_GIHA:
         
         sorted_ids = sorted(mu_hat.keys(), key=lambda k: mu_hat[k], reverse=True)
         P_hat_m = set(sorted_ids[:self.m])
-<<<<<<< HEAD
-        
-        # ... (Compute Gaps & Ambiguities) ...
-        best_pi = None
-        best_pi_dagger = None
-        max_GI = -np.inf
-
-        for pi in sorted_ids:
-            min_ambiguity = np.inf
-            current_competitor = None
-            
-            for pi_prime in sorted_ids:
-                if pi == pi_prime: continue
-                
-                g_diff = self.g_pi[pi] - self.g_pi[pi_prime]
-                width_sq = np.dot(g_diff.T, np.dot(self.V_inv, g_diff))
-                # Avoid numerical zero
-                if width_sq < 1e-12: width_sq = 1e-12
-                
-                gap = mu_hat[pi] - mu_hat[pi_prime]
-                ambiguity = (gap**2) / width_sq
-                
-                if ambiguity < min_ambiguity:
-                    min_ambiguity = ambiguity
-                    current_competitor = pi_prime
-            
-            if min_ambiguity > max_GI:
-                max_GI = min_ambiguity
-                best_pi = pi
-                best_pi_dagger = current_competitor
-
-        # ... (Stopping Condition) ...
-        worst_in_top = sorted_ids[self.m - 1]
-        best_in_rest = sorted_ids[self.m]
-        
-        g_diff_stop = self.g_pi[worst_in_top] - self.g_pi[best_in_rest]
-        w_stop = beta_t * np.sqrt(np.dot(g_diff_stop.T, np.dot(self.V_inv, g_diff_stop)))
-        delta_stop = mu_hat[worst_in_top] - mu_hat[best_in_rest]
-        
-        if (delta_stop - w_stop) >= -self.epsilon:
-            return True, sorted_ids[:self.m]
-=======
 
         top_ids = list(sorted_ids[:self.m])
         rest_ids = list(sorted_ids[self.m:])
@@ -236,23 +179,10 @@ class P_GIHA:
                 best_pi_dagger = pj
 
 
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
 
         # ... (Step Selection & Update) ...
         target_g = self.g_pi[best_pi] - self.g_pi[best_pi_dagger]
         
-<<<<<<< HEAD
-        scores = []
-        for s in range(self.num_unique_steps):
-            x_s = self.features[s]
-            v_inv_x = np.dot(self.V_inv, x_s)
-            
-            num = (np.dot(target_g.T, v_inv_x))**2
-            den = 1 + np.dot(x_s.T, v_inv_x)
-            scores.append(num / den)
-            
-        step_to_pull = np.argmax(scores)
-=======
         # Build step candidates based on mode
         if self.step_pool_mode == "paths":
             # Restrict to steps in the two boundary paths (pi* ∪ pi†)
@@ -279,7 +209,6 @@ class P_GIHA:
         step_to_pull = int(cand[int(np.argmax(scores))])
 
 
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
 
         # Update
         y_obs = oracle_callback(step_to_pull)
@@ -298,8 +227,4 @@ class P_GIHA:
         self.theta_hat = self.theta_hat + gain_vector * pred_error
 
         self.t += 1
-<<<<<<< HEAD
         return False, sorted_ids[:self.m]
-=======
-        return False, sorted_ids[:self.m]
->>>>>>> 240196b (Update the code: Now there are two options, one is all for searching in all steps for pulling and another one is paths for searching among the boundry paths.)
