a59017d455d24a736e4b5db56528103602970111
VenkteshV <vevi4591@dsv.su.se>
2026-03-03 17:15:17 +0100

    added handling for looping case

--- GenPRM/src/run_PRM_over_top_m.py
+++ GenPRM/src/run_PRM_over_top_m.py
@@ -69,7 +69,7 @@ prm = ThinkPRM(
     n=1
 )
 
-embedder = SentenceTransformer("all-MiniLM-L6-v2",device="cuda:0")
+embedder = SentenceTransformer("all-MiniLM-L6-v2",device="cuda")
 
 
 # ---------------------------
@@ -231,7 +231,6 @@ class PRMEnvironment:
             questions=[self.question],
             prefix_steps_batch=[prefix_steps]
         )[0]
-        print("prefix",res["prefix_score"])
         return float(res["prefix_score"])
 
 
@@ -259,13 +258,15 @@ def run_prm_experiment(question, paths, answers, q_idx):
     previous_J = None
     t=0
     done = False
+    previous_topms = []
     while not done and p_giha.patience <10:
         converged, top_m = p_giha.select_and_update(env.oracle_callback)
-        if previous_J == top_m:
+        if previous_J == top_m or ( t > 2 and (previous_topms[-1] == top_m or previous_topms[-2] == top_m)):
             p_giha.patience+=1
         else:
             p_giha.patience=0
         previous_J = top_m
+        previous_topms.append(previous_topms)
         
         print(f"Iter {t} | Top paths: {top_m}")
         t+=1
@@ -297,7 +298,8 @@ if __name__ == "__main__":
     exact_match = 0
     total=0
     iterations_data = {"qid":[],"iter":[]}
-    for idx in range(len(data["answer"])):
+    for idx in range(len(data["answer"][269:])):
+        idx1 = idx + 269
         start = time.time()
         print("\n============================")
         print(f"QUESTION {idx}")
@@ -310,7 +312,7 @@ if __name__ == "__main__":
 
 
         exact_match += em
-        iterations_data["qid"].append(idx+1)
+        iterations_data["qid"].append(idx+269+1)
         iterations_data["iter"].append(iterat)
         total += 1
         end = time.time()
@@ -318,6 +320,6 @@ if __name__ == "__main__":
 
         print("EM so far:", exact_match / total, mean(times))
         iterations_data_1 = pd.DataFrame(iterations_data)
-        iterations_data_1.to_csv("qid_iterations_mathodyssey_over_top_m.csv",index=False)
+        iterations_data_1.to_csv("qid_iterations_mathodyssey_over_top_m_269.csv",index=False)
 
     print("Final EM:", (exact_match / total), mean(times),stdev(times))
